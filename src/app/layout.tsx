bafd4fafd407e39a08b2cf92725f3165073f2494
gfor-gour <talukdargour.bd@gmail.com>
2024-08-26 00:03:45 +0600

    added database,worked on creating cases

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        {children}</body>
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">{children}</div>
+          <Footer/>
+        </main>
+        <Toaster/>
+        </body>
     </html>
   );
 }
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -135,6 +135,9 @@ function ReviewGrid(){
             msPerPixel={10}
             />
         </>) : null}
+        <div className="pointer-events-none absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-slate-100"/>
+        <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-100"/>
+
     </div>
 }
 
